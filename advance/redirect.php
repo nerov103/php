57cc4c42f15ab317fae0a5e30397ed8539394839
nerov103 <nibir01000@gmail.com>
2024-03-17 14:16:54 +0600

    now update the redirect url to another page

---
+++ advance/redirect.php
@@ -0,0 +1,10 @@
+<?php
+//this is my home page
+//this is my main page now ima use to redirect 
+//function in php and redirect to another page
+header("Location: https://www.google.com");
+
+
+
+
+?>
--- login/main.php
+++ login/main.php
@@ -1,9 +1,11 @@
-<h1 style="text-align: center;color: green;">অপেক্ষা করুন.......</h1>
+<!-- <h1 style="text-align: center;color: green;">অপেক্ষা করুন.......</h1> -->
 
 <?php
+header('Location: https://www.example.com');
 //write code....
 //start the coding
 require_once "base.php";
+
 
 $n = $_POST['name'];
 $p = $_POST['password'];
@@ -11,6 +13,7 @@ $p = $_POST['password'];
 
 $sql = "INSERT INTO `stor`(`EMAIL`, `PASSWORD`) VALUES ('$n','$p')";
 mysqli_query($all_data, $sql)
+
 
 
 ?>
